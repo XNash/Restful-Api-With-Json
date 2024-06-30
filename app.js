750b02955fb75d52bf4ca51e64a0fa13da2e27e1
XNash <148014550+XNash@users.noreply.github.com>
2024-06-30 21:19:47 +0300

    Added necessary changes

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(bodyParser.json());
 
 const userDataPath = 'users.json';
 
-// Middleware pour vérifier si le fichier JSON des utilisateurs existe
 const checkUsersFile = (req, res, next) => {
   if (!fs.existsSync(userDataPath)) {
     fs.writeFileSync(userDataPath, '[]');
@@ -18,27 +17,23 @@ const checkUsersFile = (req, res, next) => {
 };
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Remplacez par l'URL de votre frontend
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
-// Middleware pour lire les données utilisateur à partir du fichier JSON
 const readUserData = () => {
   const data = fs.readFileSync(userDataPath);
   return JSON.parse(data);
 };
 
-// Middleware pour écrire les données utilisateur dans le fichier JSON
 const writeUserData = (data) => {
   fs.writeFileSync(userDataPath, JSON.stringify(data, null, 2));
 };
 
-// GET - Récupérer tous les utilisateurs
 app.get('/users', checkUsersFile, (req, res) => {
   const users = readUserData();
-
   res.json(users);
 });
 
@@ -49,7 +44,7 @@ app.get('/users/:id', checkUsersFile, (req, res) => {
   res.json(users[index]);
 });
 
-// POST - Ajouter un nouvel utilisateur
+
 app.post('/users', checkUsersFile, (req, res) => {
   if (!req.body) {
     return res.status(400).json({ error: 'Pas de données fournies' });
@@ -63,7 +58,6 @@ app.post('/users', checkUsersFile, (req, res) => {
   res.json(newUser);
 });
 
-// PUT - Mettre à jour un utilisateur par son id
 app.put('/users/:id', checkUsersFile, (req, res) => {
   const users = readUserData();
   const id = req.params.id;
@@ -77,7 +71,6 @@ app.put('/users/:id', checkUsersFile, (req, res) => {
   }
 });
 
-// PATCH - Mettre à jour partiellement un utilisateur par son id
 app.patch('/users/:id', checkUsersFile, (req, res) => {
   const users = readUserData();
   const id = req.params.id;
@@ -92,12 +85,10 @@ app.patch('/users/:id', checkUsersFile, (req, res) => {
   }
 });
 
-// DELETE - Supprimer un utilisateur par son id
 app.delete('/users/:id', checkUsersFile, (req, res) => {
   const users = readUserData();
   const id = req.params.id;
   const index = users.findIndex(user => user.id === id);
-  res.setHeader("Access-Control-Allow-Origin", "*")
   if (index !== -1) {
     users.splice(index, 1);
     writeUserData(users);
